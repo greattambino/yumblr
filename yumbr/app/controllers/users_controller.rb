2a9e813299c0e09f47201fec782956d533eb4135
greattambino <marctambara@gmail.com>
2015-10-13 10:54:54 -0700

    finish user authentication and static page

---
+++ yumbr/app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :require_signed_in!,  only: [:show]
+  before_action :require_signed_out!, only: [:new, :create]
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    if current_user.nil?
+      redirect_to new_session_url
+      return
+    end
+
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :first_name,
+                                 :last_name, :password)
+  end
+end
---
+++ yumbr/app/models/user.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  validates :username, :email, :first_name, :last_name,
+            :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, :username, :email, uniqueness: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user.try(:is_password?, password) ? user : nil
+  end
+
+  def password=(unencrypted_password)
+    if unencrypted_password.present?
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(unencrypted_password)
+    end
+  end
+
+  def is_password?(unencrypted_password)
+    BCrypt::Password.new(self.password_digest).is_password?(unencrypted_password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+end

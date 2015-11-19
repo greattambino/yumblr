2da0aa167f25d51f8d83d54453525217f74a0b9b
greattambino <marctambara@gmail.com>
2015-11-19 13:19:04 -0800

    add query for categories

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,6 +1,11 @@
 class Api::CategoriesController < ApplicationController
   def index
-    @categories = Category.all
-    render json: index
+    if params[:search]
+      @categories = Category.find_by_substring(params[:search])
+    else
+      @categories = Category.all
+    end
+    
+    render json: @categories
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,12 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true, uniqueness: {case_sensitive: false}
-
-  before_save :capitalize_category
-
   has_many :food_categories, dependent: :destroy
   has_many :food_items, through: :food_categories, source: :food_item
+  before_save :capitalize_category
+
+  def self.find_by_substring(str)
+    Category.where("LOWER(name) LIKE '%#{str.downcase}%'")
+  end
 
   private
 

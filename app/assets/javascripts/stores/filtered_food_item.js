0f71ece404994c5228fd4aa1f9b6bbd02369d396
greattambino <marctambara@gmail.com>
2015-11-19 13:17:12 -0800

    clean up commented out code

--- app/assets/javascripts/stores/filtered_food_item.js
+++ app/assets/javascripts/stores/filtered_food_item.js
@@ -2,18 +2,7 @@
   'use strict';
 
   var _filteredFoodItems = [];
-  // var _searchQueryResults = [];
-  //
-  // var _params = { radius: "99999999999",
-  //                 location: { lat: 37.77493,
-  //                             lng: -122.419416,
-  //                             address: "San Francisco, CA 94103, USA"
-  //                           }
-  //               };
-
   var INITIALIZE_EVENT = "initialize";
-  // var QUERY_CHANGE_EVENT = "query_change";
-  // var PARAMS_CHANGE_EVENT = "params_change";
   var CHANGE_EVENT = "change";
   var i = 0;
 
@@ -22,28 +11,17 @@
     i = 0;
   };
 
-  // var resetQueryResults = function(foodItems) {
-  //   _searchQueryResults = foodItems.slice();
-  // };
-
   var FilteredFoodItemStore = root.FilteredFoodItemStore = $.extend({}, EventEmitter.prototype, {
     all: function() {
       return _filteredFoodItems.slice();
     },
-
-    // queryResults: function() {
-    //   return _searchQueryResults.slice();
-    // },
-    //
-    // params: function(){
-    //   return $.extend({}, _params);
-    // },
 
     next: function () {
       if (i + 1 > _filteredFoodItems.length) { i = 0; }
       // var result = _currentFoodItem;
       var result = _filteredFoodItems[i];
       i++;
+      
       return result;
     },
 
@@ -64,22 +42,6 @@
       this.removeListener(CHANGE_EVENT, callback);
     },
 
-    // addQueryListener: function(callback) {
-    //   this.on(QUERY_CHANGE_EVENT, callback);
-    // },
-    //
-    // addParamsListener: function(callback) {
-    //   this.on(PARAMS_CHANGE_EVENT, callback);
-    // },
-    //
-    // removeParamsListener: function(callback){
-    //   this.removeListener(PARAMS_CHANGE_EVENT, callback);
-    // },
-    //
-    // removeQueryListener: function(callback) {
-    //   this.removeListener(QUERY_CHANGE_EVENT, callback);
-    // },
-
     dispatcherID: AppDispatcher.register(function(payload) {
       switch(payload.actionType){
         case FoodItemConstants.FOOD_ITEMS_RECEIVED:
@@ -90,23 +52,7 @@
           resetFilteredFoodItems(payload.filteredFoodItems);
           FilteredFoodItemStore.emit(CHANGE_EVENT);
           break;
-        // case FilterConstants.RADIUS_RECEIVED:
-        //   _params.radius = payload.radius;
-        //   // FilteredFoodItemStore.emit(CHANGE_EVENT);
-        //   break;
-        // case FilterConstants.LOCATION_RECEIVED:
-        //   _params.location = payload.location;
-        //   FilteredFoodItemStore.emit(PARAMS_CHANGE_EVENT);
-        //   break;
-        // case FilterConstants.QUERY_RECEIVED:
-        //   resetQueryResults(payload.queryResults);
-        //   FilteredFoodItemStore.emit(QUERY_CHANGE_EVENT);
-        //   break;
       }
-        // case FilteredFoodItemConstants.FILTERED_FOOD_ITEM_RECEIVED:
-        //   resetFilteredFoodItems(payload.filteredFoodItem);
-        //   FilteredFoodItemStore.emit(QUERY_CHANGE_EVENT);
-        //   break;
     })
   });
 })(this);

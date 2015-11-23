808c15ae6717fc7471699622caf2d833558bd1e6
greattambino <marctambara@gmail.com>
2015-11-22 23:04:58 -0800

    fix n+1 queries on FoodItem::find_by_food_and_cuisine

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -31,23 +31,30 @@ class FoodItem < ActiveRecord::Base
   def self.find_by_food_and_location(str, location, radius)
     radius = radius.to_f
     idx = -1
-    FoodItem.includes(:restaurant).where("LOWER(name) LIKE '%#{str.downcase}%'").select do |item|
-      idx += 1
-      item.restaurant.distance(location, idx) <= (radius)
+    FoodItem
+      .includes(:restaurant)
+      .where("LOWER(name) LIKE '%#{str.downcase}%'")
+      .select do |item|
+        idx += 1
+        item.restaurant.distance(location, idx) <= (radius)
     end
   end
 
   def self.find_by_food_and_cuisine(str, cuisine_id)
-    Cuisine.find(cuisine_id).food_items.where("LOWER(food_items.name) LIKE '%#{str.downcase}%'")
+    Cuisine.find(cuisine_id).food_items
+      .includes(:restaurant)
+      .where("LOWER(food_items.name) LIKE '%#{str.downcase}%'")
   end
 
   def self.find_by_all_params(str, cuisine_id, location, radius)
     radius = radius.to_f
-    # restaurants = []
     idx = -1
-    Cuisine.find(cuisine_id).food_items.includes(:restaurant).where("LOWER(food_items.name) LIKE '%#{str.downcase}%'").select do |item|
-      idx += 1
-      item.restaurant.distance(location, idx) <= (radius)
+    Cuisine.find(cuisine_id).food_items
+      .includes(:restaurant)
+      .where("LOWER(food_items.name) LIKE '%#{str.downcase}%'")
+      .select do |item|
+        idx += 1
+        item.restaurant.distance(location, idx) <= (radius)
     end
     # food_items = Cuisine.find(cuisine_id).food_items.includes(:restaurant).where("LOWER(food_items.name) LIKE '%#{str.downcase}%'").select do |item, idx|
     #   sleep(1) if idx % 10 == 0

5e4fdd5b1d6dabf742a987f02a65b50cea8530d9
greattambino <marctambara@gmail.com>
2015-11-24 18:06:01 -0800

    update generate btn

--- app/assets/javascripts/components/search/search_generate_btn.js.jsx
+++ app/assets/javascripts/components/search/search_generate_btn.js.jsx
@@ -1,14 +1,22 @@
 (function(root) {
   'use strict';
-
+  var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+  var OverlayTrigger = ReactBootstrap.OverlayTrigger;
+  var Popover = ReactBootstrap.Popover;
   var GenerateBtn = root.GenerateBtn = React.createClass({
     mixins: [ReactRouter.History],
 
     render: function () {
       return(
-        <button className="btn btn-default btn-circle btn-lg">
-          <span><img src="http://res.cloudinary.com/yumblr/image/upload/v1446014758/app/icon-small.png"/></span>
-        </button>
+        <ButtonToolbar>
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Popover title="Popover bottom"><strong>Holy guacamole!</strong> Check this info.</Popover>}>
+            <button className="btn btn-default btn-circle btn-lg generate-btn">
+              <span><img src="http://res.cloudinary.com/yumblr/image/upload/v1448393942/app/yumblr-icon17.png"/></span>
+            </button>
+          </OverlayTrigger>
+        </ButtonToolbar>
       );
     }
   });

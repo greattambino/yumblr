4ad8a71ea97412c50a31f7ddcc2f6d9d69996ccb
greattambino <marctambara@gmail.com>
2015-12-06 23:11:03 -0800

    update google map api key

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def root
+    @google = "https://maps.googleapis.com/maps/api/js?key=#{ENV['google_maps_key']}&libraries=geometry,places"
+    render :root
   end
 end

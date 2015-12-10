8071ed273fe65f56e0e2b8107cf1a331a6122b0f
greattambino <marctambara@gmail.com>
2015-12-09 23:36:15 -0800

    create Review Constants

---
+++ app/assets/javascripts/constants/review_constants.js
@@ -0,0 +1,8 @@
+(function (root) {
+  'user strict';
+
+  var ReviewConstants = root.ReviewConstants = {
+    REVIEWS_RECEIVED: "REVIEWS_RECEIVED",
+    REVIEW_RECEIVED: "REVIEW_RECEIVED"
+  };
+})(this);

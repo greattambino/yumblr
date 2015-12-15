bd1ce113880614fdbe75f3dbe27268a90059a7e0
greattambino <marctambara@gmail.com>
2015-12-14 18:27:39 -0800

    modify Rating Component to listen on the Review Store

--- app/assets/javascripts/components/reviews/rating.js.jsx
+++ app/assets/javascripts/components/reviews/rating.js.jsx
@@ -3,14 +3,40 @@
 
   var Rating = root.Rating = React.createClass({
     getInitialState: function () {
-      return { rate: 0 };
+      return {
+        foodItemId: this.props.foodItemId,
+        rating: 0,
+        reviewCount: 0,
+        hoverRating: 0
+      };
+    },
+    componentDidMount: function () {
+      ReviewStore.addChangeListener(this._onChange);
+      ReviewApiUtil.fetchAllReviews(this.props.foodItemId);
+    },
+
+    componentWillUnmount: function () {
+      ReviewStore.removeChangeListener(this._onChange);
+    },
+
+    componentWillReceiveProps: function (newProps) {
+      if (newProps.foodItemId !== this.props.foodItemId) {
+        ReviewApiUtil.fetchAllReviews(newProps.foodItemId);
+      }
+    },
+
+    _onChange: function () {
+      this.setState({
+        rating: ReviewStore.averageScore(this.props.foodItemId),
+        reviewCount: ReviewStore.reviewCount(this.props.foodItemId)
+      });
     },
 
     renderReadOnly: function () {
       this.stars = [];
       var fullStars = 0,
           halfStars = 0,
-          rating = ((this.props.rating || 0) * 2) / 2;
+          rating = (Math.round(this.state.rating * 2) / 2) || 0;
       if (rating % 1 === 0) {
         fullStars = rating;
       } else {
@@ -21,37 +47,37 @@
         this.stars.push(<li key={i + 'full'} className="star on" />);
       }
       for (var j = 0; j < halfStars; j++) {
-        this.stars.push(<li key={j + 'half'} className="half star on" />);
+        this.stars.push(<li key={j + 'half'} className="half star" />);
       }
       for (var k = 0; k < (5 - (fullStars + halfStars)); k++) {
         this.stars.push(<li key={k + 'empty'} className="star" />);
       }
 
       var reviews = "reviews";
-      if (this.props.rating === 1) { reviews = "review"; }
+      if (this.state.reviewCount === 1) { reviews = "review"; }
 
       this.stars.push(
         <span className="review-count" key="review-count">
-          {this.props.reviewCount} {reviews}
+          {this.state.reviewCount} {reviews}
         </span>
       );
     },
 
     handleMouseOver: function (e) {
       e.preventDefault();
-      this.setState({ rate: e.currentTarget.value });
+      this.setState({ hoverRating: e.currentTarget.value });
     },
 
     handleMouseOut: function (e) {
       e.preventDefault();
-      this.setState({ rate: this.props.rating });
+      this.setState({ hoverRating: this.props.rating });
     },
 
     renderClickable: function() {
       this.stars = [];
       for (var i = 1; i <= 5; i++) {
         var handleClick = this.props.onClick.bind(null, i);
-        if (this.state.rate >= i) {
+        if (this.state.hoverRating >= i) {
           this.stars.push(
             <li key={i}
               value={i}

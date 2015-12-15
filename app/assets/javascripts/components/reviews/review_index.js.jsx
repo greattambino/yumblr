0d6fbc9e0ca3af56e244abc356977f3714b07d79
greattambino <marctambara@gmail.com>
2015-12-14 19:00:55 -0800

    update ReviewIndex rendering contents

--- app/assets/javascripts/components/reviews/review_index.js.jsx
+++ app/assets/javascripts/components/reviews/review_index.js.jsx
@@ -3,7 +3,7 @@
 
   var ReviewIndex = root.ReviewIndex = React.createClass({
     getInitialState: function () {
-      return ({ reviews: ReviewStore.all() });
+      return ({ reviews: ReviewStore.reviews(this.props.foodItemId) });
     },
 
     componentDidMount: function () {
@@ -14,34 +14,20 @@
       ReviewStore.removeChangeListener(this._onChange);
     },
 
-    _onChange: function (reviews) {
-      this.setState({ reviews: reviews });
+    _onChange: function () {
+      this.setState({ reviews: ReviewStore.reviews(this.props.foodItemId) });
     },
 
     render: function () {
       return(
         <ul id="sortable" className="list-unstyled ui-sortable">
-            <strong className="pull-left primary-font">Klaythompson11</strong>
-            <small className="pull-right text-muted">
-               <span className="glyphicon glyphicon-time"> </span>7 mins ago</small>
-            <br/>
-            <li className="ui-state-default">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
-            <br/>
-             <strong className="pull-left primary-font">Draymondgreen23</strong>
-            <small className="pull-right text-muted">
-               <span className="glyphicon glyphicon-time"></span>14 mins ago</small>
-            <br/>
-            <li className="ui-state-default">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+          {this.state.reviews.map(function (review) {
+            return(
+              <ReviewIndexItem key={review.id} review={review} />
+            );
+          }.bind(this))}
         </ul>
       );
     }
   });
 })(this);
-
-// <ul>
-//   {this.state.reviews.map(function (review) {
-//     return(
-//       <li key={review.id}>{review.body}</li>
-//     );
-//   })}
-// </ul>

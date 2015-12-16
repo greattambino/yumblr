4246e1d62b60245a9c1ca4d6ebe7128f4d34137f
greattambino <marctambara@gmail.com>
2015-12-15 19:51:10 -0800

    Modify to render either reviewForm or currentUserReviews based on if user has reviewed

--- app/assets/javascripts/components/reviews/review.js.jsx
+++ app/assets/javascripts/components/reviews/review.js.jsx
@@ -34,10 +34,82 @@
 
     },
 
+    renderReviewForm: function () {
+      return(
+        <div className="review-form">
+          <div className="review-rating-container" >
+            {this.props.foodItem.name}
+            <Rating
+              rating={this.state.rating}
+              readOnly={false}
+              onClick={this.handleRatingChange}
+              reviewCount={this.props.reviewCount}
+            />
+          </div>
+          <div className="row">
+            <div className="span12">
+              <form id="review-input-form"
+                className="form-search form-horizontal"
+                onSubmit={this.handleSubmit}>
+                <div className="input-append span12">
+                  <textarea
+                    className={"review-input-text" + focus}
+                    valueLink={this.linkState("reviewBody")}
+                    placeholder="Write a review" /><br/>
+                  <input type="submit" className="btn-review-save" value="Submit" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    },
+
+    renderUserReviews: function () {
+      var currentUser = UserStore.currentUser(),
+          currentUserUsername = currentUser.username,
+          currentUserReviews = currentUser.reviews,
+          reviewCount = currentUserReviews.length,
+          foodItemId = this.props.foodItem.id;
+
+      return(
+        <div className="user-review-container">
+          <div className="user-review-food-item-header">{this.props.foodItem.name}</div>
+          <div className="user-review-food-item-sub-header">Your recent reviews</div>
+          <div className="row">
+            <div className="span12">
+              <ul id="sortable" className="list-unstyled ui-sortable">
+                {currentUserReviews.map(function (review) {
+                  if (review.food_item_id === foodItemId) {
+                    return(
+                      <ReviewIndexItem
+                        key={"user-reviews-" + review.id}
+                        readOnly={false}
+                        author={currentUserUsername}
+                        review={review} />
+                    );
+                  }
+                }.bind(this))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    },
+
     render: function () {
       var focus = "";
       if (this.state.reviewBody.length > 0) {
         focus = " focus";
+      }
+      var reviewHeader,
+          currentUserId = UserStore.currentUser().id,
+          foodItemId = this.props.foodItem.id;
+
+      if (UserStore.currentUserHasReviewed(foodItemId)) {
+        reviewHeader = this.renderUserReviews();
+      } else {
+        reviewHeader = this.renderReviewForm();
       }
 
       return (
@@ -56,31 +128,8 @@
               </Modal.Title>
             </Modal.Header>
             <Modal.Body id="review-modal-content">
-              <div className="review-rating-container" >
-                {this.props.foodItem.name}
-                <Rating
-                  rating={this.state.rating}
-                  readOnly={false}
-                  onClick={this.handleRatingChange}
-                  reviewCount={this.props.reviewCount}
-                />
-              </div>
 
-              <div className="row">
-                <div className="span12">
-                  <form id="review-input-form"
-                    className="form-search form-horizontal"
-                    onSubmit={this.handleSubmit}>
-                    <div className="input-append span12">
-                      <textarea
-                        className={"review-input-text" + focus}
-                        valueLink={this.linkState("reviewBody")}
-                        placeholder="Write a review" /><br/>
-                      <input type="submit" className="btn-review-save" value="Submit" />
-                    </div>
-                  </form>
-                </div>
-              </div>
+              {reviewHeader}
 
               <div className="review-modal-index">
                 <ReviewIndex foodItemId={this.props.foodItem.id} />

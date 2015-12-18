0b4a02d2bf5a2d12c2acadb5d8240d7d54eb1899
greattambino <marctambara@gmail.com>
2015-12-17 17:18:50 -0800

    clear existing timers on handleLike and set timeout id's

--- app/assets/javascripts/components/likes/like_btn.js.jsx
+++ app/assets/javascripts/components/likes/like_btn.js.jsx
@@ -2,34 +2,39 @@
   'use strict';
 
   var LikeBtn = root.LikeBtn = React.createClass({
-    getInitialState: function () {
-      return {likeState: "", hover: false, disabled: false, status: ""};
+    getInitialState: function() {
+      return {
+        likeState: "",
+        hover: false,
+        disabled: false
+      };
     },
 
-    componentDidMount: function () {
+    componentDidMount: function() {
       UserStore.addChangeListener(this.setLike);
-      this.unlike = <span className="glyphicon glyphicon-heart"/>;
-      this.like = <span className="glyphicon glyphicon-heart-empty"/>;
+      this.unlike = <span className="glyphicon glyphicon-heart" />;
+      this.like = <span className="glyphicon glyphicon-heart-empty" />;
       this.setLike(this.props);
       $('.heart-container').hide(1);
     },
 
-    componentWillReceiveProps: function (nextProps) {
+    componentWillReceiveProps: function(nextProps) {
       this.setLike(nextProps);
     },
 
-    componentWillUnmount: function () {
+    componentWillUnmount: function() {
       UserStore.removeChangeListener(this.setLike);
     },
-    handleMouseOver: function () {
+
+    handleMouseOver: function() {
       this.setState({hover: true});
     },
 
-    handleMouseOut: function () {
+    handleMouseOut: function() {
       this.setState({hover: false});
     },
 
-    setLike: function (props) {
+    setLike: function(props) {
       if (typeof props === "undefined") {
         props = this.props;
       }
@@ -43,26 +48,32 @@
       }
     },
 
-    handleLike: function () {
+    handleLike: function() {
       var likableType = this.props.likableType,
           likableId   = this.props.likableId;
 
       if (this.state.likeState === this.like) {
         LikeApiUtil.createLike(likableType, likableId);
         $('.heart-container').fadeIn(800).delay(500).fadeOut(600);
-        setTimeout(function() {
+        // clear any existing timers
+        if (this._timer !== null) {
+          clearTimeout(this._timer);
+        }
+        this._timer = setTimeout(function() {
           this.setState({likeState: this.unlike});
+          this._timer = null;
         }.bind(this), 200);
       } else {
         LikeApiUtil.destroyLike(likableType, likableId);
-        setTimeout(function() {
+        this._timer = setTimeout(function() {
           this.setState({likeState: this.like});
+          this._timer = null;
         }.bind(this), 200);
       }
       ApiUtil.fetchCurrentUser(window.CURRENT_USER);
     },
 
-    render: function () {
+    render: function() {
       var likeState = this.state.likeState,
           numLikes  = this.props.numLikes,
           klass,
@@ -79,7 +90,7 @@
         }
       }
 
-      return (
+      return(
         <div className="like-container">
           <div className="heart-container">
             <img src="./assets/heart.png"></img>

342269414454f94d6067fe7c5a8e992391ec4421
greattambino <marctambara@gmail.com>
2015-12-19 22:17:15 -0800

    add ERRORS_RECEIVED constant

--- app/assets/javascripts/constants/error_constants.js
+++ app/assets/javascripts/constants/error_constants.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var ErrorConstants = root.ErrorConstants = {
-    ERRORS_RECEIVED: "ERRORS_RECEIVED"
+    ERRORS_RECEIVED: "ERRORS_RECEIVED",
+    ERRORS_CLEARED: "ERRORS_CLEARED"
   };
 })(this);

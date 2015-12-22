bc898e6ab2422ca29585f6e62242be4d09dae774
greattambino <marctambara@gmail.com>
2015-12-21 23:46:39 -0800

    fix typo in users path

--- app/assets/javascripts/yumblr.js.jsx
+++ app/assets/javascripts/yumblr.js.jsx
@@ -30,7 +30,7 @@ $(function(){
 
   var path = window.location.pathname;
 
-  if (path !== "/users/"    && path !== "/session" &&
+  if (path !== "/users"    && path !== "/session" &&
       path !== "/users/new" && path !== "/session/new") {
     React.render(<Router>{routes}</Router>, root);
   }

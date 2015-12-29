0645f0f73be26b11be8c89606bd75af5eeffd9e7
greattambino <marctambara@gmail.com>
2015-12-28 23:53:31 -0800

    add REVIEW_UPDATED constant

--- app/assets/javascripts/constants/review_constants.js
+++ app/assets/javascripts/constants/review_constants.js
@@ -3,6 +3,7 @@
 
   var ReviewConstants = root.ReviewConstants = {
     REVIEWS_RECEIVED: "REVIEWS_RECEIVED",
-    REVIEW_RECEIVED: "REVIEW_RECEIVED"
+    REVIEW_RECEIVED: "REVIEW_RECEIVED",
+    REVIEW_UPDATED: "REVIEW_UPDATED"
   };
 })(this);

1e55d48bac5c6d5365b8cba6494ab2bd70afecaf
greattambino <marctambara@gmail.com>
2015-12-29 22:02:21 -0800

    refactor for styling fix

--- app/assets/javascripts/components/reviews/user_review_index.js.jsx
+++ app/assets/javascripts/components/reviews/user_review_index.js.jsx
@@ -14,12 +14,6 @@
       ReviewStore.removeChangeListener(this._onChange);
     },
 
-    // componentWillReceiveProps: function(newProps) {
-    //   if (this.props !== newProps) {
-    //     this.setState({ reviews: ReviewStore.reviews(newProps.foodItemId) });
-    //   }
-    // },
-
     _onChange: function() {
       this.setState({ reviews: ReviewStore.reviews(this.props.foodItemId) });
     },
@@ -27,37 +21,24 @@
     render: function() {
       return(
         <div className="user-review-container">
-          <div className="user-review-food-item-header">{this.props.foodItemName}</div>
-          <div className="user-review-food-item-sub-header">Your recent reviews</div>
-          <div className="row">
-            <div className="span12">
-              <ul id="sortable" className="list-unstyled ui-sortable">
-                {this.state.reviews.map(function(review) {
-                  if (review.user_id === this.props.currentUserId) {
-                    return(
-                      <ReviewIndexItem
-                        key={"user-reviews-" + review.id}
-                        readOnly={false}
-                        author={this.props.currentUserUsername}
-                        review={review} />
-                    );
-                  }
-                }.bind(this))}
-              </ul>
-            </div>
+          <div className="user-review-food-item-sub-header">
+            Your recent reviews
           </div>
+          <ul id="sortable" className="list-unstyled ui-sortable">
+            {this.state.reviews.map(function(review) {
+              if (review.user_id === this.props.currentUserId) {
+                return(
+                  <ReviewIndexItem
+                    key={"user-reviews-" + review.id}
+                    readOnly={false}
+                    author={this.props.currentUserUsername}
+                    review={review} />
+                );
+              }
+            }.bind(this))}
+          </ul>
         </div>
       );
     }
   });
 })(this);
-// {this.props.currentUserReviews.map(function(review) {
-//   if (review.food_item_id === this.props.foodItemId) {
-//     return(
-//       <ReviewIndexItem
-//         key={"user-reviews-" + review.id}
-//         readOnly={false}
-//         author={this.props.currentUserUsername}
-//         review={review} />
-//     );
-//   }
